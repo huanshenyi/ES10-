f547a1671c9487b02a8272565405c3d008c39443
huanshenyi <txy1226052@gmail.com>
2019-12-22 09:01:42 +0900

    2-5 over

---
+++ static/lesson2-2.js
@@ -0,0 +1,26 @@
+// {
+//   let a = 1
+//   console.log(a)
+// }
+// // console.log(a)
+// var b = 3
+// let c = 4
+//
+// console.log(b, c)
+// console.log(window.b, window.c)
+//
+// var b = 4
+// console.log(b)
+
+// let c = 5
+// console.log(c)
+
+// const a = 2
+// console.log(a)
+// console.log(b)
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i)
+  }, 1000)
+}
