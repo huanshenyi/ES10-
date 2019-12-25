3081db087a4d9ed824b5728a4b4ccc5981adde26
huanshenyi <txy1226052@gmail.com>
2019-12-25 21:20:29 +0900

    2-14 static method over

--- js/lesson2-3.js
+++ js/lesson2-3.js
@@ -16,24 +16,39 @@
 // }
 // dog.eat()
 // monkey.eat()
-let _age = 4
+// let _age = 4
+// class Animal {
+//   constructor (type) {
+//     this.type = type
+//   }
+//   get age () {
+//     return _age
+//   }
+//   set age (val) {
+//     if (val < 7) {
+//       _age = val
+//     }
+//   }
+//   eat () {
+//     console.log('i am eat food')
+//   }
+// }
+// let dog = new Animal('dog')
+// console.log(dog.age)
+// dog.age = 5
+// console.log(dog.age)
+
 class Animal {
   constructor (type) {
     this.type = type
   }
-  get age () {
-    return _age
-  }
-  set age (val) {
-    if (val < 7) {
-      _age = val
-    }
-  }
   eat () {
+    Animal.walk()
     console.log('i am eat food')
+  }
+  static walk () {
+    console.log('i am walking')
   }
 }
 let dog = new Animal('dog')
-console.log(dog.age)
-dog.age = 5
-console.log(dog.age)
+dog.eat()
